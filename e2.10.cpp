273c973fd8f67aa64c7d588a01077d4528fe8787
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-08-26 21:28:41 -0400

    upload

--- e2.10.cpp
+++ e2.10.cpp
@@ -4,19 +4,21 @@ using namespace std;
 
 int main()
 {
-	cout << "Enter number of gallons: " << endl;
-	int numgal = 0;
+	cout << "Enter number of gallons: ";
+	float numgal = 0;
 	cin >> numgal;
 
-	cout << "Enter fuel efficency in miles per gallon: " << endl;
-	int mpg = 0;
+	cout << "Enter fuel efficency in miles per gallon: ";
+	float mpg = 0;
 	cin >> mpg;
 
-	cout << "Enter price of gas per gallon: " << endl;
+	cout << "Enter price of gas per gallon: ";
 	float price = 0;
 	cin >> price;
 
-	int x = (price * numgal) / (100/mpg);
-	cout << "Your trip will cost " << x << " dollars per 100 miles." << endl;
+	float costPer100 = (100/mpg)*price;
+	float distance = numgal * mpg; 
+	cout << "Your trip will cost " << costPer100 << " dollars per 100 miles. You will travel " << distance << " miles." << endl;
+
 	return 0;
 }
